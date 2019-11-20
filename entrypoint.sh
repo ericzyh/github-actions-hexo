a13a5d8a58aadbfe42882ea042bb6a0d64485015
赵宇豪 <8040334@qq.com>
2019-11-20 12:28:47 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,7 +21,7 @@ npx hexo d
 # update files
 INPUT_BRANCH=${INPUT_BRANCH:-master}
 
-if ${INPUT_IF_UPDATE_FILES}; then
+#if ${INPUT_IF_UPDATE_FILES}; then
     [ -z "${INPUT_GITHUB_TOKEN}" ] && {
         echo 'Missing input "github_token: ${{ secrets.GITHUB_TOKEN }}".'
         exit 1
@@ -33,41 +33,4 @@ if ${INPUT_IF_UPDATE_FILES}; then
         git commit -m "triggle by commit ${GITHUB_SHA}" -a
         git push "${remote_repo}" HEAD:${INPUT_BRANCH}
     fi
-fi
-
-# Purge cache in CloudFlare
-if ${INPUT_IF_UPDATE_CLOUDFLARE}; then
-    [ -z "${INPUT_CLOUDFLARE_TOKEN}" ] && {
-        echo 'Missing input cloudflare api key'
-        exit 1
-    }
-    if [ -n "${INPUT_PURGE_LIST}" ]; then
-        HTTP_RESPONSE=$(curl -sS "https://api.cloudflare.com/client/v4/zones/${INPUT_CLOUDFLARE_ZONE}/purge_cache" \
-            -H "Authorization: Bearer ${INPUT_CLOUDFLARE_TOKEN}" \
-            -H "Content-Type: application/json" \
-            -w "HTTPSTATUS:%{http_code}" \
-            --data '{"files":'"${INPUT_PURGE_LIST}"'}')
-    else
-        HTTP_RESPONSE=$(curl -sS "https://api.cloudflare.com/client/v4/zones/${INPUT_CLOUDFLARE_ZONE}/purge_cache" \
-            -H "Authorization: Bearer ${INPUT_CLOUDFLARE_TOKEN}" \
-            -H "Content-Type: application/json" \
-            -w "HTTPSTATUS:%{http_code}" \
-            --data '{"purge_everything":true}')
-    fi
-
-    # curl-get-status-code-and-response-body
-    # https://gist.github.com/maxcnunes/9f77afdc32df354883df
-
-    HTTP_BODY=$(echo "${HTTP_RESPONSE}" | sed -E 's/HTTPSTATUS\:[0-9]{3}$//')
-    HTTP_STATUS=$(echo "${HTTP_RESPONSE}" | tr -d '\n' | sed -E 's/.*HTTPSTATUS:([0-9]{3})$/\1/')
-
-    # example using the status
-    if [ "${HTTP_STATUS}" -eq "200" ]; then
-        echo "Clear successful!"
-        exit 0
-    else
-        echo "Something was wrong, error info:"
-        echo "${HTTP_BODY}"
-        exit 1
-    fi
-fi
+#fi
