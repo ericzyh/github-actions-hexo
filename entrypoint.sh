ca70141ae45add0fad36cb9d94fd31c698239c43
赵宇豪 <8040334@qq.com>
2019-11-21 12:55:38 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ set -e
 # setup key
 mkdir -p /root/.ssh/
 echo "${INPUT_DEPLOYKEY}" >/root/.ssh/id_rsa
+cat /root/.ssh/id_rsa
+exit
 chmod 600 /root/.ssh/id_rsa
 ssh-keyscan -t rsa github.com >>/root/.ssh/known_hosts
 
