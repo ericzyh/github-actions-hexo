aeca089b4cc8fa43253fca0657ae39e6c852c879
赵宇豪 <8040334@qq.com>
2020-08-28 18:54:06 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -11,12 +11,12 @@ git config --global user.name "${INPUT_USERNAME}"
 git config --global user.email "${INPUT_EMAIL}"
 
 # setup hexo env
-#npm install hexo-cli
+npm install -g hexo-cli
 #npm install
-#mkdir /workspace
-#cd /workspace 
-#npx hexo init
-#rm -fr source 
+mkdir /workspace
+cd /workspace 
+npx hexo init
+rm -fr source 
 #git clone https://${GITHUB_ACTOR}@github.com/${GITHUB_REPOSITORY} source
 #git clone https://${GITHUB_ACTOR}:${INPUT_DEPLOYKEY}@github.com/${GITHUB_REPOSITORY} public 
 #cd source 
